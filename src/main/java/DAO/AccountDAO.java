68481dd463fc93d0cac77ad35e61dbe58f35cbe0
Edwin Hernandez <edwinhdz442@gmail.com>
2023-02-09 02:08:29 +0000

    AccountDao logic

---
+++ src/main/java/DAO/AccountDAO.java
@@ -0,0 +1,71 @@
+package DAO;
+
+import Model.Account;
+import Util.ConnectionUtil;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AccountDAO{
+
+    public List<Account> getAllAccounts(){
+        Connection connection = ConnectionUtil.getConnection();
+        List <Account> accounts = new ArrayList<>();
+        try{
+            String sql = "SELECT * FROM account";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            ResultSet rs = preparedStatement.executeQuery();
+                while(rs.next()){
+                    Account account = new Account(rs.getInt("account_id"), 
+                    rs.getString("username"), 
+                    rs.getString("password"));
+                    accounts.add(account);
+                }
+            }
+            catch(SQLException e){
+                System.out.println(e.getMessage());
+            }
+            return accounts;
+    }
+
+    public Account getAccount(int account_id, String username, String password){
+        Connection connection = ConnectionUtil.getConnection();
+        try{
+            String sql = "SELECT * FROM account WHERE account_id = ? AND username = ? AND password = ?";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+
+            preparedStatement.setInt(1, account_id);
+            preparedStatement.setString(2, username);
+            preparedStatement.setString(3, password);
+
+            ResultSet rs = preparedStatement.executeQuery();
+            while(rs.next()){
+                Account account = new Account(rs.getInt("account_id"),
+                rs.getString("username"), 
+                rs.getString("password"));
+                return account;
+            }
+        }catch(SQLException e){
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
+
+    public Account insertAccount(Account account){
+        Connection connection = ConnectionUtil.getConnection();
+        try{
+            String sql = "INSERT INTO account (username, password) VALUES (?, ?)";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+
+            preparedStatement.setString(1, account.getUsername());
+            preparedStatement.setString(2, account.getPassword());
+
+            preparedStatement.executeUpdate();
+            return account;
+        }catch(SQLException e){
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
+}
